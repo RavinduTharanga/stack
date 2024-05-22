2442a0bb44209733962e4926a802dedda3c738b7
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2024-05-22 11:05:35 -0400

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Set the environment variable for non-interactive installation
+export DEBIAN_FRONTEND=noninteractive
+
+# Update package lists
 sudo apt-get update
-sudo apt-get install -y nginx
+
+# Install Apache and its dependencies
+sudo apt-get install -y apache2
+
+# Any other dependencies
+# sudo apt-get install -y <other-packages>
